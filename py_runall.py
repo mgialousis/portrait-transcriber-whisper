60e7f23783e67db9c05598aa9a636cbea1bd8c1c
Miltiadis Gialousis <mgialousis@hotmail.com>
2025-06-30 22:12:59 +0200

    Latest users

--- py_runall.py
+++ py_runall.py
@@ -14,7 +14,10 @@ def run_pipeline_on_folder(folder: Path):
 def main():
     base_dir = Path("/Users/miltos/Desktop/ftp_portrait/PORTRAIT")
     exclude_dir = base_dir / "backup_original"
-    include_dir = [ base_dir / "LHE12", base_dir / "1Y2UK" , base_dir / "C1KA2" ]
+    include_dir = [ base_dir / "2PA1L", base_dir / "2HGA1", base_dir / "XZ21K", base_dir / "21YYD",
+base_dir / "1H2GG", base_dir / "Z2ZF1", base_dir / "V2DL1", base_dir / "UQ2M1",
+base_dir / "F21XO", base_dir / "YN21I", base_dir / "VD2X1", base_dir / "CY2H1",
+base_dir / "YSW21" ]
 
     subfolders = [f for f in base_dir.iterdir() if f.is_dir() and f in include_dir]
 
